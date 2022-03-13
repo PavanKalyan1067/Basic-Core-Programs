c73718632ef83045c320cdc5269f6a434b5f0006
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-03-14 00:09:02 +0530

    Added files

---
+++ Leapyear1/Leapyear1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0)
+            {
+                if ((year % 100 == 0) && (year % 400 != 0))
+                    Console.WriteLine("{0} is not a leap year", year);
+                else
+                    Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+                Console.WriteLine("{0} is not a leap year", year);
+        }
+    }
+}
